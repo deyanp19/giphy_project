bd3471950c2f5638490eec9e548aa2d2395e8f28
deyan petrov <deyanp19@hotmail.com>
2018-04-27 20:45:30 -0500

    buttons created

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var topics = ["rambo", "katy perry", "construcktion", "fil"]
+var topics = ["rambo", "katy perry", "construction", "fail", "dogs"]
 for (var i = 0; i < topics.length; i++) {
     var topicsBtn = $("<button>");
      var btnDiv = $("#buttonsArea");
@@ -12,63 +12,41 @@ for (var i = 0; i < topics.length; i++) {
 $("button").on("click", function() {
 //create a variable that will take the topick of the buttons or the content of the topics[]
 
-// var gifContent =$(this).attr("topics-Name")
-// //javascript, jQuery
-// var queryURL = $.get("http://api.giphy.com/v1/gifs/search?q=" + gifContent + "&api_key=GmZqHjeK15F7FZADmseIJAqZ0GvgcRSR&limit=10");
+var gifContent =$(this).attr("topics-Name")
+//javascript, jQuery
+var queryURL = $.get("http://api.giphy.com/v1/gifs/search?q=" + gifContent + "&api_key=GmZqHjeK15F7FZADmseIJAqZ0GvgcRSR&limit=10");
 
-// //queryURL.done(function(data) { console.log("success got data", data); });
-// $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-//       console.log(queryURL);
+queryURL.done(function(data) { console.log("success got data", data); });
+$.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+      console.log(queryURL);
 
-//       console.log(response);
-//   })
+      console.log(response);
+ 
+var results = response.data; // this is the array in the object data
+//createa for loop to check the results
+for (var i = 0; i < results.length; i++) {
+    // creating the storage for the images in div's
+    var giphydiv = $("<div>");
+    var p = $("<p>").text("Rating" + results[i].rating);
+    var giphyImage = $("<img>");
+    // setting the src attribute of the image to a property pulled off the result item
+    giphyImage.attr("src", results[i].images.fixed_height.url);
 
- // Grabbing and storing the data-animal property value from the button
- var animal = $(this).attr("data-animal");
+    // to attach the paragrapsh(p) to the div
+    // i dont understand why the images dont appear on the screen . The object consoles log
+    giphydiv.append(p);
+    giphydiv.append(giphyImage);
+    $("#gifs-appear-here").prepend(giphydiv);
+    }
+}); 
 
- // Constructing a queryURL using the animal name
- var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-   animal + "&api_key=dc6zaTOxFJmzC&limit=10";
-
- // Performing an AJAX request with the queryURL
- $.ajax({
-   url: queryURL,
-   method: "GET"
- })
-   // After data comes back from the request
-   .then(function(response) {
-     console.log(queryURL);
-
-     console.log(response);
-     // storing the data from the AJAX request in the results variable
-     var results = response.data;
-
-     // Looping through each result item
-     for (var i = 0; i < results.length; i++) {
-
-       // Creating and storing a div tag
-       var animalDiv = $("<div>");
-
-       // Creating a paragraph tag with the result item's rating
-       var p = $("<p>").text("Rating: " + results[i].rating);
-
-       // Creating and storing an image tag
-       var animalImage = $("<img>");
-       // Setting the src attribute of the image to a property pulled off the result item
-       animalImage.attr("src", results[i].images.fixed_height.url);
-
-       // Appending the paragraph and image tag to the animalDiv
-       animalDiv.append(p);
-       animalDiv.append(animalImage);
-
-       // Prependng the animalDiv to the HTML page in the "#gifs-appear-here" div
-       $("#gifs-appear-here").prepend(animalDiv);
-     }
-   });
-
-
+// Needs to create a search box and push the values into the array i created at the begining. This will generate new buttons.
+var val = $("#search").val();
+     
+      topics.push(val);
 });
-alert('hui');
+
+
