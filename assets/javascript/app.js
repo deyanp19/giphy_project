03841bf0df84c76c4dfdcc0abf82baedf3197b98
deyan petrov <deyanp19@hotmail.com>
2018-05-01 23:21:42 -0500

    fix

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,5 @@
-var topics = ["rambo", "katy perry", "construction", "fail", "dogs"]
+var topics = ["rambo", "katy perry", "construction", "fail", "dogs", "sambo", "cat"];
+
 for (var i = 0; i < topics.length; i++) {
     var topicsBtn = $("<button>");
      var btnDiv = $("#buttonsArea");
@@ -9,7 +10,8 @@ for (var i = 0; i < topics.length; i++) {
 }
 
 
-$("button").on("click", function() {
+$(document).on("click", "button", function(event) {
+    event.preventDefault();
 //create a variable that will take the topick of the buttons or the content of the topics[]
 
 var gifContent =$(this).attr("topics-Name")
@@ -31,13 +33,13 @@ console.log(results);
 for (var i = 0; i < results.length; i++) {
     // creating the storage for the images in div's
     var giphydiv = $("<div>");
-    var p = $("<p>").text("Rating" + results[i].rating);
+    var p = $("<p>").text("Rating: " + results[i].rating);
     var giphyImage = $("<img>");
     // setting the src attribute of the image to a property pulled off the result item
     giphyImage.attr("src", results[i].images.fixed_height.url);
 
     // to attach the paragrapsh(p) to the div
-    // i dont understand why the images dont appear on the screen . The object consoles log
+    
     giphydiv.append(p);
     giphydiv.append(giphyImage);
     $("#gifs-appear-here").prepend(giphydiv);
@@ -45,9 +47,20 @@ for (var i = 0; i < results.length; i++) {
 }); 
 
 // Needs to create a search box and push the values into the array i created at the begining. This will generate new buttons.
-var val = $("#search").val();
-     
-      topics.push(val);
+
+//$("input[type='text']").val(""); 
+
 });
 
+$("#add-to-do").on("click", function(event) {
+    event.preventDefault();
+var newButton = $("input[type='text']").val().trim();
+topics.push(newButton);
+var topicsBtn = $("<button>");
+     var btnDiv = $("#buttonsArea");
+    topicsBtn.addClass("topic-" + i);
+    topicsBtn.attr("topics-Name", newButton);
+    topicsBtn.text(newButton);
+    btnDiv.append(topicsBtn);
 
+});
